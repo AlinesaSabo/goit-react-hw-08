d46dbde104075c38ca058c9bd416e8c70b3f7322
Alinesa <alinesa322@gmail.com>
2024-10-20 21:47:06 +0300

    add icon

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,17 @@
 import { NavLink } from "react-router-dom";
 import s from "./Navigation.module.css";
+import { IoMdHome } from "react-icons/io";
+import { HiPhone } from "react-icons/hi";
 
 const Navigation = () => {
   return (
-    <nav>
+    <nav className={s.wrapper}>
       <NavLink to="/" className={s.link}>
+        <IoMdHome />
         Home
       </NavLink>
       <NavLink to="/contacts" className={s.link}>
+        <HiPhone />
         Contact
       </NavLink>
     </nav>
